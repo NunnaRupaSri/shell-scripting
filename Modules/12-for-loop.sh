5c84d1e8983667ddc249c8b5182c2ced413cc8e6
acer <rupasri797@gmail.com>
2020-05-04 08:21:39 +0530

    add line

---
+++ Modules/12-for-loop.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# for loop is going to deal with a list of items
+ITEMS='shipping user cart'
+
+for items in $ITEMS ; do
+  echo "Restarting service $item"
+  systemctl restart $item
+  do
