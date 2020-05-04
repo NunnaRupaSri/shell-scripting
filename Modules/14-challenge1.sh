d62ab35600b03d8ff28fb60d6a39997afe53753e
acer <rupasri797@gmail.com>
2020-05-04 11:34:30 +0530

    add line

--- Modules/14-challenge1.sh
+++ Modules/14-challenge1.sh
@@ -2,7 +2,12 @@
 
 read -p 'Enter the value : ' value
 
+i=$value
 while [ $value -gt 0 ]; do
+  while [ $i gt 0 ]; do
+    echo "$i"
+    i=$(($i-1))
+    done
   echo "$value"
   value=$(($value-1))
-  done
+done
